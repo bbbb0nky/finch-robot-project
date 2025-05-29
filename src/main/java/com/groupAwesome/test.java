0b8a464218099ecbc2054fc53179871abd3d5425
Tahlia <highschool.t.tran@gmail.com>
2025-05-29 14:22:15 -0700

    added code

---
+++ src/main/java/com/groupAwesome/test.java
@@ -0,0 +1,11 @@
+package com.groupawesome;
+
+import com.birdbrain.Finch;
+
+public class test {
+    public static void main(String[] args) {
+        Finch myFinch = new Finch();
+
+        myFinch.playNote(60,0.5);
+    }
+}
