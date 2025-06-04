3be5c4e8e5e0686c8305e17609b3178258f2522d
Tahlia <highschool.t.tran@gmail.com>
2025-06-04 12:36:31 -0700

    full code

--- src/main/java/com/groupAwesome/test.java
+++ src/main/java/com/groupAwesome/test.java
@@ -5,7 +5,19 @@ import com.birdbrain.Finch;
 public class test {
     public static void main(String[] args) {
         Finch myFinch = new Finch();
-
-        myFinch.playNote(60,0.5);
+        int count = 0;
+        while(count < 5){
+        for (int i = 0; i < 3; i++) {
+            myFinch.setMove("F", 2, 100);
+            myFinch.setTurn("L", 20, 100);
+            myFinch.setMove("B", 2, 100);
+            myFinch.setTurn("R", 20, 100);
+        }
+        
+        myFinch.setTurn("L", 360, 100);
+        count++;
+    }
+        myFinch.stopAll();
+        myFinch.disconnect();
     }
 }
